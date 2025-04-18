b482958d09fcfcfeccf9fb1e4c8fff0a690864dc
Prosperiko <prosperossaiku.com>
2025-04-18 16:47:42 +0100

    .

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FINC:app
--- app.py
+++ app.py
@@ -1,36 +1,8 @@
 from flask import Flask, render_template, request, redirect, session, flash, jsonify, redirect, url_for, send_file
-import mysql.connector
-from mysql.connector import Error
-from flask_bcrypt import Bcrypt
-import sqlite3
-import re
-from flask_mail import Mail, Message
-import random
-from itsdangerous import URLSafeTimedSerializer
-from datetime import datetime
-import os
-from flask_socketio import SocketIO, emit
-from email.mime.text import MIMEText
-import pandas as pd
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from dotenv import load_dotenv
-load_dotenv()
-import os
-from werkzeug.utils import secure_filename
-from datetime import datetime, timezone
-import tempfile
-
-# Setting up the API key and model
-
-import google.generativeai as genai
 
 
 app = Flask(__name__, template_folder='template', static_folder='static')
-bcrypt = Bcrypt(app)
+
 
 @app.route('/')
 def index():
